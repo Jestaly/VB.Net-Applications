ce567ea137a8f73d4ca73378d0a7e64e9438c7aa
Jzy <jestalycastillo15@gmail.com>
2024-10-10 20:47:10 +0800

    added OOP activity VB.NET

---
+++ GradeInterface/GradeInterface.Designer.vb
@@ -0,0 +1,38 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class GradeInterface
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' GradeInterface
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        IsMdiContainer = True
+        Name = "GradeInterface"
+        Text = "Grade Interface"
+        ResumeLayout(False)
+    End Sub
+
+End Class
---
+++ GradeInterface/GradeInterface.vb
@@ -0,0 +1,3 @@
+﻿Public Class GradeInterface
+
+End Class
