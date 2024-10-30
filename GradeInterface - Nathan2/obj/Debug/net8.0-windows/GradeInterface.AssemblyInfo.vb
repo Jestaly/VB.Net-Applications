eade5b216e2817b1870f70df4997773908cc9bb6
Jzy <jestalycastillo15@gmail.com>
2024-10-30 15:06:57 +0800

    commit

--- GradeInterface - Nathan2/obj/Debug/net8.0-windows/GradeInterface.AssemblyInfo.vb
+++ GradeInterface - Nathan2/obj/Debug/net8.0-windows/GradeInterface.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("GradeInterface"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0339807bd4a3bc56dcc36ec64961897ab8f47a51"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b8d7c71a8ad2a4cba8e0ed8201bc7b237981bd"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("GradeInterface"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("GradeInterface"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
